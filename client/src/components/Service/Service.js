d7c845f5abd1374dcb14c8057bf7c83cafca08e1
Robert Schmidt <r.schmidt@wackenhut.de>
2021-07-02 09:26:37 +0200

    worked on service page

--- client/src/components/Service/Service.js
+++ client/src/components/Service/Service.js
@@ -6,8 +6,60 @@ const Service = props => {
     const {setMenu} = props
 
     return <>
-                <h1>Hi from Service</h1>
-                <button type="button" className="btn btn-outline-danger" onClick={() => setMenu(Menu.INIT)}>Back</button>
+               <button type="button" className="btn btn-outline-primary btn-sm btn-block" onClick={() => setMenu(Menu.INIT)}>Back</button>
+               <br/>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <h1>Webapplications from Zero to Finish</h1><br/>
+                    </li>
+                    <li class="list-group-item">
+                        <h4>Consulting, Planing, Prototyping and Deploying</h4>
+                        <br/>
+                        <p>fdsfadsfas</p>
+                    </li>
+                    <li class="list-group-item">
+                        <h4>Testing, Logs and Maintenance</h4>
+                        <br/>
+                        <p>To make your business keep rolling, Your App will be deployed with a high test coverage to make your business keep rolling. If something went wrong, I will fix it</p>
+                    </li>
+                    <li class="list-group-item">
+                        <h4>Know-how in Mechanical Engineering, Business Economics and Jurisprudence</h4>
+                        <br/>
+                        <p>As an owner of a Masters Degree in Mechanical Engineering (M.Eng) and a Bachelors Degree in Business Law (LL.B), I can especially provide Coding-Solutions in these fields of business.</p>
+                    </li>
+                    <li class="list-group-item">
+                        <h4>Your problems solved!</h4>
+                        <br/>
+                        <p>Programming isn´t just a tool to write handy applications. It can be used to solve a huge amout of problems that could arise in a business.<br/>
+                        Do you have the feeling that programming can help you on a certain task? Get in touch with me and I see what I can do for you! 
+                        </p>
+                    </li>
+                </ul>
+                
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <h1>Technical Details</h1><br/>
+                        <p>I´m open get into and work with any technology! The tech-stack I use the most and keep at state of the art is:</p>
+                        <table class="table table-borderless">
+                            <thead>
+                            <tr>
+                                <th>Frontend</th>
+                                <th>Backend</th>
+                                <th>Database</th>
+                                <th>Other</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>React.js (ES6 JavaScript), SASS, Bootstrap</td>
+                                <td>PHP (Rest-Api, MVC-Approach)</td>
+                                <td>MySQL or similar, MongoDB (rarely)</td>
+                                <td>usually Python or PHP</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                </ul>
             </>
 }
 
